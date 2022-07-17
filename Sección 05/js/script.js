13eebdfa9e65b482cbe7a2e54384b78e9899789d
diegoncollazo <diego.nicolas@outlook.com.ar>
2022-07-17 17:35:30 -0300

    TypeScript

---
+++ Sección 05/js/script.js
@@ -0,0 +1,10 @@
+var nombre = "Diego Collazo";
+
+function mostrarNombre() {
+	// var nombre = "Nicolás"
+	nombre = "Nicolás";
+	console.log("Dentro:", nombre);
+}
+
+mostrarNombre();
+console.log("Fuera:", nombre);
